00c67e169ac3cd2ffb6602be55b6c199dda2c0c8
Jordan Ruhle <jordanruhle@gmail.com>
2022-11-25 22:20:35 -0800

    first commit

---
+++ first-java-program/ContactInfo.java
@@ -0,0 +1,7 @@
+public class ContactInfo {
+    public static void main(String[] args) {
+        System.out.println("My name is Jordan");
+        System.out.println("I am 34 years old");
+        System.out.println("My Hometown is ");
+    }
+}
